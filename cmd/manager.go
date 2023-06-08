1afa3c02d23f6bc043eee19a2dc21ce44615460e
thomasscothamilton <>
2023-06-08 17:56:32 -0400

    adding more updates

--- cmd/manager.go
+++ cmd/manager.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -13,13 +12,7 @@ import (
 // managerCmd represents the manager command
 var managerCmd = &cobra.Command{
 	Use:   "manager",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "start the manager",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("manager called")
 	},
--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"math/rand"
-	"os"
-	"time"
-
 	"github.com/thomasscothamilton/thomasscothamilton/cmd"
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	if err := cmd.Execute(); err != nil {
-		os.Exit(1)
-	}
+	cmd.Execute()
 }
